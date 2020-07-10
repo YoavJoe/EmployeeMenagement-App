9466338992e20d4ae5a1ebadf67c0cfa93f642a5
YoavJoe <yoav.shalev67@gmail.com>
2020-07-10 12:05:35 +0300

    adding stack navigator

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import {SignIn} from './App/Screens';
-
+import { SignIn, CreateAccount } from './App/Screens';
 
 const AuthStack = createStackNavigator();
 
 export default () => (
-  <NavigationContainer>
-    <AuthStack.Navigator>
-      <AuthStack.Screen name='SignIn' component = {SignIn} />
-    </AuthStack.Navigator>
-  </NavigationContainer>
+    <NavigationContainer>
+      <AuthStack.Navigator headerMode='none'>
+        <AuthStack.Screen name='SignIn' 
+        component = {SignIn} 
+        options={{title: "Sign In"}}
+        />
+        <AuthStack.Screen name='CreateAccount' 
+        component = {CreateAccount}
+        options={{title: "Create Account"}} />
+      </AuthStack.Navigator>
+    </NavigationContainer>
 );
+
