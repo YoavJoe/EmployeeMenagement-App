5919d63002856a874d24dccdfcb96d852fb05f06
YoavJoe <yoav.shalev67@gmail.com>
2020-07-11 17:49:57 +0300

    adding firebase config

---
+++ config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+export const firebaseConfig = {
+    apiKey: "AIzaSyB_bcpR0gdp342AyfxZt_nTt7vlb6g0KF8",
+    authDomain: "employeemanagement-app.firebaseapp.com",
+    databaseURL: "https://employeemanagement-app.firebaseio.com",
+    projectId: "employeemanagement-app",
+    storageBucket: "employeemanagement-app.appspot.com",
+    messagingSenderId: "282108812282",
+    appId: "1:282108812282:web:e3882ec762ebb7075ca243",
+    measurementId: "G-NE5VV9NGQ6"
+  };
